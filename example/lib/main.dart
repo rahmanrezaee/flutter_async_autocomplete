004e47f2e234422e6337c6a99945fcd08733da19
Rahman Rezaee <r.razaee@webfume.com>
2022-12-27 17:53:08 +0430

    make all functionality

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,115 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_async_autocomplete/flutter_async_autocomplete.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var autoController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      title: 'Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(title: Text('Example')),
+          body: Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: AsyncAutocomplete<Country>(
+              controller: autoController,
+              onTapItem: (Country country) {
+                autoController.text = country.name;
+                print("selected Country ${country.name}");
+              },
+              suggestionBuilder: (data) => ListTile(
+                title: Text(data.name),
+                subtitle: Text(data.code),
+              ),
+              asyncSuggestions: (searchValue) => getCountry(searchValue),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Future<List<Country>> getCountry(String search) async {
+    List<Country> countryList = [
+      Country(name: 'Afghanistan', code: 'AF'),
+      Country(name: 'Åland Islands', code: 'AX'),
+      Country(name: 'Albania', code: 'AL'),
+      Country(name: 'Algeria', code: 'DZ'),
+      Country(name: 'American Samoa', code: 'AS'),
+      Country(name: 'AndorrA', code: 'AD'),
+      Country(name: 'Angola', code: 'AO'),
+      Country(name: 'Anguilla', code: 'AI'),
+      Country(name: 'Antarctica', code: 'AQ'),
+      Country(name: 'Antigua and Barbuda', code: 'AG'),
+      Country(name: 'Argentina', code: 'AR'),
+      Country(name: 'Armenia', code: 'AM'),
+      Country(name: 'Aruba', code: 'AW'),
+      Country(name: 'Australia', code: 'AU'),
+      Country(name: 'Austria', code: 'AT'),
+      Country(name: 'Azerbaijan', code: 'AZ'),
+      Country(name: 'Bahamas', code: 'BS'),
+      Country(name: 'Bahrain', code: 'BH'),
+      Country(name: 'Bangladesh', code: 'BD'),
+      Country(name: 'Barbados', code: 'BB'),
+      Country(name: 'Belarus', code: 'BY'),
+      Country(name: 'Belgium', code: 'BE'),
+      Country(name: 'Belize', code: 'BZ'),
+      Country(name: 'Benin', code: 'BJ'),
+      Country(name: 'Bermuda', code: 'BM'),
+      Country(name: 'Bhutan', code: 'BT'),
+      Country(name: 'Bolivia', code: 'BO'),
+      Country(name: 'Bosnia and Herzegovina', code: 'BA'),
+      Country(name: 'Botswana', code: 'BW'),
+      Country(name: 'Bouvet Island', code: 'BV'),
+      Country(name: 'Brazil', code: 'BR'),
+      Country(name: 'British Indian Ocean Territory', code: 'IO'),
+      Country(name: 'Brunei Darussalam', code: 'BN'),
+      Country(name: 'Bulgaria', code: 'BG'),
+      Country(name: 'Burkina Faso', code: 'BF'),
+      Country(name: 'Burundi', code: 'BI'),
+      Country(name: 'Cambodia', code: 'KH'),
+      Country(name: 'Cameroon', code: 'CM'),
+      Country(name: 'Canada', code: 'CA'),
+      Country(name: 'Cape Verde', code: 'CV'),
+      Country(name: 'Cayman Islands', code: 'KY'),
+      Country(name: 'Central African Republic', code: 'CF'),
+      Country(name: 'Chad', code: 'TD'),
+      Country(name: 'Chile', code: 'CL'),
+    ];
+
+    await Future.delayed(const Duration(microseconds: 500));
+    return countryList
+        .where((element) =>
+            element.name.toLowerCase().startsWith(search.toLowerCase()))
+        .toList();
+  }
+
+  onChange(value) {
+    //  <Country>(value) {
+    setState(() {
+      autoController.text = value.name;
+    });
+    print('onChanged value: ${value.name}');
+  }
+}
+
+class Country {
+  String name;
+  String code;
+
+  Country({required this.code, required this.name});
 }
--- lib/flutter_async_autocomplete.dart
+++ lib/flutter_async_autocomplete.dart
@@ -1,7 +1,344 @@
+// Copyright 2021 4inka
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 library flutter_async_autocomplete;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+// typedef SuggestionBuilder<T> = ;
+// typedef OnTapItem<T> = Function(T data);
+
+class AsyncAutocomplete<T> extends StatefulWidget {
+  /// Fetches list of suggestions from a Future
+  final Future<List<T>> Function(String searchValue)? asyncSuggestions;
+
+  /// Text editing controller
+  final TextEditingController? controller;
+
+  /// Can be used to decorate the input
+  final InputDecoration decoration;
+
+  /// Function that handles the changes to the input
+  final Function(String)? onChanged;
+
+  /// Function that handles the submission of the input
+  final Function(String)? onSubmitted;
+
+  /// Function that handles the Tap of the Items
+  final Function(T data)? onTapItem;
+
+  /// Can be used to set custom inputFormatters to field
+  final List<TextInputFormatter> inputFormatter;
+
+  /// Can be used to set the textfield initial value
+  final T? initialValue;
+
+  /// Can be used to set the text capitalization type
+  final TextCapitalization textCapitalization;
+
+  /// Determines if should gain focus on screen open
+  final bool autofocus;
+
+  /// Can be used to set different keyboardTypes to your field
+  final TextInputType keyboardType;
+
+  /// Can be used to manage TextField focus
+  final FocusNode? focusNode;
+
+  /// Can be used to set a custom color to the input cursor
+  final Color? cursorColor;
+
+  /// Can be used to set custom style to the suggestions textfield
+  final TextStyle inputTextStyle;
+
+  /// Can be used to set custom style to the suggestions list text
+  final TextStyle suggestionTextStyle;
+
+  /// Can be used to set custom background color to suggestions list
+  final Color? suggestionBackgroundColor;
+
+  /// Used to set the debounce time for async data fetch
+  final Duration debounceDuration;
+
+  /// Can be used to customize suggestion items
+  // final Widget Function(T data) suggestionBuilder;
+  final Widget Function(T data) suggestionBuilder;
+
+  /// Can be used to display custom progress idnicator
+  final Widget progressIndicatorBuilder;
+
+  /// Can be used to validate field value
+  final String Function(String?)? validator;
+
+  /// Creates a autocomplete widget to help you manage your suggestions
+  const AsyncAutocomplete(
+      {required this.asyncSuggestions,
+      required this.suggestionBuilder,
+      this.progressIndicatorBuilder = const CircularProgressIndicator(),
+      this.controller,
+      this.decoration = const InputDecoration(),
+      this.onChanged,
+      this.onTapItem,
+      this.onSubmitted,
+      this.inputFormatter = const [],
+      this.initialValue,
+      this.autofocus = false,
+      this.textCapitalization = TextCapitalization.sentences,
+      this.keyboardType = TextInputType.text,
+      this.focusNode,
+      this.cursorColor,
+      this.inputTextStyle = const TextStyle(),
+      this.suggestionTextStyle = const TextStyle(),
+      this.suggestionBackgroundColor,
+      this.debounceDuration = const Duration(milliseconds: 400),
+      this.validator})
+      : assert(onChanged != null || controller != null,
+            'onChanged and controller parameters cannot be both null at the same time'),
+        assert(!(controller != null && initialValue != null),
+            'controller and initialValue cannot be used at the same time'),
+        assert(asyncSuggestions == null || asyncSuggestions != null,
+            'suggestions and asyncSuggestions cannot be both null or have values at the same time');
+
+  @override
+  State<AsyncAutocomplete<T>> createState() => _AsyncAutocompleteState<T>();
+}
+
+class _AsyncAutocompleteState<T> extends State<AsyncAutocomplete<T>> {
+  final LayerLink _layerLink = LayerLink();
+  late TextEditingController _controller;
+  bool _hasOpenedOverlay = false;
+  bool _isLoading = false;
+  OverlayEntry? _overlayEntry;
+  List<T> _suggestions = [];
+  Timer? _debounce;
+  String _previousAsyncSearchText = '';
+  late FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = widget.focusNode ?? FocusNode();
+    _controller = widget.controller ?? TextEditingController(text: '');
+    _controller.addListener(() => updateSuggestions(_controller.text));
+    _focusNode.addListener(() {
+      if (_focusNode.hasFocus)
+        openOverlay();
+      else
+        closeOverlay();
+    });
+  }
+
+  void openOverlay() {
+    if (_overlayEntry == null) {
+      RenderBox renderBox = context.findRenderObject() as RenderBox;
+      var size = renderBox.size;
+      var offset = renderBox.localToGlobal(Offset.zero);
+
+      _overlayEntry ??= OverlayEntry(
+          builder: (context) => Positioned(
+              left: offset.dx,
+              top: offset.dy + size.height + 5.0,
+              width: size.width,
+              child: CompositedTransformFollower(
+                  link: _layerLink,
+                  showWhenUnlinked: false,
+                  offset: Offset(0.0, size.height + 5.0),
+                  child: FilterableList(
+                      loading: _isLoading,
+                      suggestionBuilder: widget.suggestionBuilder,
+                      progressIndicatorBuilder: widget.progressIndicatorBuilder,
+                      items: _suggestions,
+                      maxListHeight: 500,
+                      suggestionTextStyle: widget.suggestionTextStyle,
+                      suggestionBackgroundColor:
+                          widget.suggestionBackgroundColor,
+                      onItemTapped: (value) {
+                        widget.onTapItem?.call(value);
+                        closeOverlay();
+                        _focusNode.unfocus();
+                      }))));
+    }
+    if (!_hasOpenedOverlay) {
+      Overlay.of(context)!.insert(_overlayEntry!);
+      setState(() => _hasOpenedOverlay = true);
+    }
+  }
+
+  void closeOverlay() {
+    if (_hasOpenedOverlay) {
+      _overlayEntry!.remove();
+      setState(() {
+        _previousAsyncSearchText = '';
+        _hasOpenedOverlay = false;
+      });
+    }
+  }
+
+  Future<void> updateSuggestions(String input) async {
+    rebuildOverlay();
+    if (widget.asyncSuggestions != null) {
+      setState(() => _isLoading = true);
+      if (_debounce != null && _debounce!.isActive) _debounce!.cancel();
+      _debounce = Timer(widget.debounceDuration, () async {
+        if (_previousAsyncSearchText != input ||
+            _previousAsyncSearchText.isEmpty ||
+            input.isEmpty) {
+          _suggestions = await widget.asyncSuggestions!(input) as List<T>;
+          setState(() {
+            _isLoading = false;
+            _previousAsyncSearchText = input;
+          });
+          rebuildOverlay();
+        }
+      });
+    }
+  }
+
+  void rebuildOverlay() {
+    if (_overlayEntry != null) {
+      _overlayEntry!.markNeedsBuild();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CompositedTransformTarget(
+        link: _layerLink,
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                  decoration: widget.decoration,
+                  controller: _controller,
+                  inputFormatters: widget.inputFormatter,
+                  autofocus: widget.autofocus,
+                  focusNode: _focusNode,
+                  textCapitalization: widget.textCapitalization,
+                  keyboardType: widget.keyboardType,
+                  cursorColor: widget.cursorColor ?? Colors.blue,
+                  style: widget.inputTextStyle,
+                  onChanged: (value) => widget.onChanged?.call(value),
+                  onFieldSubmitted: (value) {
+                    widget.onSubmitted?.call(value);
+                    closeOverlay();
+                    _focusNode.unfocus();
+                  },
+                  onEditingComplete: () => closeOverlay(),
+                  validator: widget.validator != null
+                      ? (value) => widget.validator!(value)
+                      : null // (value) {}
+                  )
+            ]));
+  }
+
+  @override
+  void dispose() {
+    if (_overlayEntry != null) _overlayEntry!.dispose();
+    if (widget.controller == null) {
+      _controller.removeListener(() => updateSuggestions(_controller.text));
+      _controller.dispose();
+    }
+    if (_debounce != null) _debounce?.cancel();
+    if (widget.focusNode == null) {
+      _focusNode.removeListener(() {
+        if (_focusNode.hasFocus)
+          openOverlay();
+        else
+          closeOverlay();
+      });
+      _focusNode.dispose();
+    }
+    super.dispose();
+  }
+}
+
+class FilterableList<T> extends StatelessWidget {
+  final List<T> items;
+  final Function(T data) onItemTapped;
+  final double elevation;
+  final double maxListHeight;
+  final TextStyle suggestionTextStyle;
+  final Color? suggestionBackgroundColor;
+  final bool loading;
+  final Widget Function(T data) suggestionBuilder;
+  final Widget? progressIndicatorBuilder;
+
+  const FilterableList(
+      {required this.items,
+      required this.onItemTapped,
+      required this.suggestionBuilder,
+      this.elevation = 5,
+      this.maxListHeight = 150,
+      this.suggestionTextStyle = const TextStyle(),
+      this.suggestionBackgroundColor,
+      this.loading = false,
+      this.progressIndicatorBuilder});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final ScaffoldState? scaffold = Scaffold.maybeOf(context);
+
+    Color _suggestionBackgroundColor = suggestionBackgroundColor ??
+        scaffold?.widget.backgroundColor ??
+        theme.scaffoldBackgroundColor;
+
+    return Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(5),
+      color: _suggestionBackgroundColor,
+      child: Container(
+        constraints: BoxConstraints(maxHeight: maxListHeight),
+        child: Visibility(
+          visible: items.isNotEmpty || loading,
+          child: ListView.builder(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(5),
+            itemCount: loading ? 1 : items.length,
+            itemBuilder: (context, index) {
+              if (loading) {
+                return Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: progressIndicatorBuilder!);
+              }
+
+              return InkWell(
+                  child: suggestionBuilder(items[index]),
+                  onTap: () => onItemTapped(items[index]));
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
